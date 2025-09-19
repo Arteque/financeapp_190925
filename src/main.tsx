0bee510801f2b53d684ca138a87ce5405a371bb0
Ahmed Lemssiah <art3que@gmail.com>
2025-09-19 14:44:18 +0200

    React Router Template and App path structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,55 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import "../styles/index.css"
+
+import Root from './templates/Root'
+import Overview from './pages/dashboard/Overview'
+import Transactions from './pages/dashboard/Transactions'
+import Budgets from './pages/dashboard/Budgets'
+import Pots from './pages/dashboard/Pots'
+import RecurringBills from './pages/dashboard/RecurringBills'
+import Login from './pages/login/Login'
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Root />,
+    errorElement:<h1>Oops! Something went wrong.</h1>,
+    children: [
+      {
+        path:"/overview",
+        element:<Overview />
+      },
+      {
+        path:"/transactions",
+        element:<Transactions />
+      },
+      {
+        path:"/budgets",
+        element:<Budgets />
+      },
+      {
+        path:"/pots",
+        element:<Pots />
+      },
+      {
+        path:"/recurring-bills",
+        element:<RecurringBills />
+      },
+      {
+        path:"/login",
+        element:<Login />
+      }
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello, world!</h1>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/templates/Root.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+const Root = () => {
+    return (
+        <>
+        <header>Header</header>
+        <main><Outlet /></main>
+        <footer>Footer</footer>
+        </>
+    )
+}
+
+export default Root; 
