9025e347d2d9cf3bf8ad38e3c2a1877e32a53565
Factum <art3que@gmail.com>
2025-09-21 19:58:50 +0200

    Added a button variant 'OrangeRed'

--- src/components/atoms/Buttons/Button.tsx
+++ src/components/atoms/Buttons/Button.tsx
@@ -9,8 +9,11 @@ const ButtonVariantStyling = {
   fulldark:
     "block p-[1rem] bg-grey-500 text-grey-100 text_sm--bold hover:bg-grey-400",
   fullbeige:
-    "block p-[1rem] bg-beige-500 text-grey-500 text_sm--bold border-2 border-transparent hover:bg-grey-100 border-grey-400 hover:border-grey-400",
-  withIcon: "flex items-center gap_12",
+    "block p-[1rem] bg-beige-500 text-grey-500 text_sm--bold border-2 border-transparent hover:bg-grey-100 hover:border-beige-200",
+  fullorangered:
+    "block p-[1rem] bg-red-100 text-grey-100 text_sm--bold border-2 border-transparent hover:bg-red-200 border-grey-100",
+  withIcon: "flex text-grey-400 items-center gap_12 text_sm",
+  withIcon_bold: "flex text-grey-400 items-center gap_12 text_sm--bold",
 };
 
 type ButtonVariantKey = keyof typeof ButtonVariantStyling;
--- src/components/templates/Root.tsx
+++ src/components/templates/Root.tsx
@@ -1,15 +1,12 @@
 import { Outlet } from "react-router-dom";
-import ButtonIcon from "../molecules/ButtonIcon";
-import LinkIcon from "../molecules/LinkIcon";
 import Button from "../atoms/Buttons/Button";
 const Root = () => {
   return (
     <>
       <header>Header</header>
-      <LinkIcon children="This is a link" />
-      <ButtonIcon children="This is a Button" />
-      <Button variant="fulldark">test</Button>
-      <Button variant="fullbeige">test</Button>
+      <Button variant="fullorangered">Test Button</Button>
+      <Button variant="fulldark">Test Button</Button>
+      <Button variant="fullbeige">Test Button</Button>
       <main>
         <Outlet />
       </main>
