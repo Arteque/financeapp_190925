812a56016d98dc30d704be76dbcade03e0f7c4db
Ahmed Lemssiah <art3que@gmail.com>
2025-10-14 10:44:45 +0200

    Installed React-hook-form zod @hookform/resolvers date-fns decimal.js recahrts react-hot-toast axios lodash-es and decimal.js

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
+
 import router from "./Router";
+
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
