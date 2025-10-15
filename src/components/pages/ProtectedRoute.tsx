a14bbefcb81c73b11bddc326d5cf53ac86633ac9
Ahmed Lemssiah <art3que@gmail.com>
2025-10-15 15:07:57 +0200

    Components Reset

--- src/components/pages/ProtectedRoute.tsx
+++ src/components/pages/ProtectedRoute.tsx
@@ -1,8 +1,7 @@
 import { Navigate} from "react-router-dom"
-import {useAuth} from "../../context/AuthContext"
 import Root from "../../template/Root"
 const ProtectedRoute = () => {
-  const  {user} = useAuth()
+  const  user = true
   return user ? <Root /> : <Navigate to="/login" />
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,17 +1,11 @@
-import { useAuth } from "../context/AuthContext"
 
 const LoginPage = () => {
 
-    const {login}  = useAuth()
-
-    const handleLogin = () => {
-        login({name:"Demo User", email:"demo@example.com"})
-    }
 
   return (
     <div>
       <h2>Login</h2>
-      <button onClick={handleLogin}>Login</button>
+      <button >Login</button>
     </div>
   )
 }
