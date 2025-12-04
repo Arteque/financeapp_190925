db5b992fc7634db0b850edfb08e4e1eae2bfbf79
Ahmed Lemssiah <art3que@gmail.com>
2025-12-04 12:53:55 +0100

    Signup  Page

--- src/components/organisms/Form/SignupForm.tsx
+++ src/components/organisms/Form/SignupForm.tsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import Button from '../../atoms/Link'
+import FormElement from '../../molecules/Forms/FormElement'
+import InputPassword from './InputPassword'
+import InputText from './InputText'
 
 const SignupForm = () => {
   return (
-    <div>SignupForm</div>
+    <FormElement >
+        <InputText id="Name" label='First-Name' placeholder='Your firstname' required className="mb-5" />
+        <InputText id="Name" label='Last-Name' placeholder='Your lasttname' required className='mb-5'/>
+        <InputText id="mail" label='E-Mail' placeholder='Your E-Mail' required className="mb-5" />
+        <InputPassword id='pass' label='Password' placeholder='Password' type='password' />
+        <InputPassword id='pass' label='Password' placeholder='Password' type='password' className='block mt-5' />
+        <Button type='submit' children="Signup" variant='primary' />
+    </FormElement>
   )
 }
 
--- src/components/templates/Forms/Signup.tsx
+++ src/components/templates/Forms/Signup.tsx
@@ -1,8 +1,10 @@
+import Heading from "../../atoms/Text/Heading/Heading"
 import SignupForm from "../../organisms/Form/SignupForm"
 
 const TheSignup = () => {
   return (
     <>
+        <Heading variant="heading_lg" children="Login" className="mb-5" />
         <SignupForm />
     </>
   )
