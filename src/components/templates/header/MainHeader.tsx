73b5a14fef3cd620f31a179f5cb6bf57f453bdbc
Factum <art3que@gmail.com>
2025-12-08 20:17:30 +0100

    Grid Element for template

--- src/components/templates/header/MainHeader.tsx
+++ src/components/templates/header/MainHeader.tsx
@@ -1,13 +1,26 @@
-import { Link } from "react-router-dom"
-
+import { Link } from "react-router-dom";
+import { useState } from "react";
 const MainHeader = () => {
+  const [loggedIn, setLoggedIn] = useState<boolean>(false);
   return (
-    <header className="bg-grey-500 rounded-b-[0.5rem] px-[2.5rem] py-[1.5rem]">
-      <Link to="/" className="block w-fit mx-auto">
+    <header className="bg-grey-500 rounded-b-lg lg:relative lg:rounded-lg overflow-hidden">
+      <Link
+        to="/"
+        className="block w-fit mx-auto lg:mx-[unset] lg:absolute z-10 p-5"
+        role="link"
+        aria-label="Go to homepage"
+      >
         <img src="/images/logo-large.svg" alt="PersonalFinance App Logo" />
       </Link>
+      {!loggedIn && (
+        <img
+          src="/images/illustration-authentication.svg"
+          alt="Illustration showing a person running towards money bills happily"
+          className="absolute inset-0 w-full z-0"
+        />
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default MainHeader
+export default MainHeader;
