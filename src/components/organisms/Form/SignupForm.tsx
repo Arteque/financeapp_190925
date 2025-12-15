662fdaf5ca2b950a8d920f9934af876933120e82
Ahmed Lemssiah <art3que@gmail.com>
2025-12-15 14:25:38 +0100

    Form Element

--- src/components/organisms/Form/SignupForm.tsx
+++ src/components/organisms/Form/SignupForm.tsx
@@ -1,17 +1,21 @@
-import Button from '../../atoms/Link'
 import FormElement from '../../molecules/Forms/FormElement'
+import ButtonElement from '../../molecules/Forms/FormField/ButtonElement'
 import InputPassword from './InputPassword'
 import InputText from './InputText'
 
-const SignupForm = () => {
+interface SignupFormProps {
+  onSubmit?: () => void;
+}
+
+const SignupForm = ({onSubmit}:SignupFormProps) => {
   return (
-    <FormElement >
+    <FormElement onSubmit={onSubmit}>
         <InputText id="Name" label='First-Name' placeholder='Your firstname' required className="mb-5" />
         <InputText id="Name" label='Last-Name' placeholder='Your lasttname' required className='mb-5'/>
         <InputText id="mail" label='E-Mail' placeholder='Your E-Mail' required className="mb-5" />
         <InputPassword id='pass' label='Password' placeholder='Password' type='password' />
         <InputPassword id='pass' label='Password' placeholder='Password' type='password' className='block mt-5' />
-        <Button type='submit' children="Signup" variant='primary' />
+        <ButtonElement type='submit'>SignUp</ButtonElement>
     </FormElement>
   )
 }
