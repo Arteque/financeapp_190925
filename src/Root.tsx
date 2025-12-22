4c5bb291e7f625413b7887533bd9a1e581c0fc31
Ahmed Lemssiah <art3que@gmail.com>
2025-12-22 10:49:19 +0100

    Cleaning up the header component

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,12 +4,8 @@ import MainHeader from "./components/templates/header/MainHeader";
 const Root = () => {
   return (
     <>
-      <div className="lg:grid lg:grid-cols-2 lg:m-5 w-full">
-        <MainHeader />
-        <main>
-          <Outlet />
-        </main>
-      </div>
+      <MainHeader />
+      <Outlet />
     </>
   );
 };
--- src/components/templates/header/MainHeader.tsx
+++ src/components/templates/header/MainHeader.tsx
@@ -1,38 +1,16 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import Heading from "../../atoms/Text/Heading/Heading";
-import MainNav from "./MainNav";
+
 const MainHeader = () => {
-  const [loggedIn, setLoggedIn] = useState<boolean>(true);
   return (
-    <header
-      className="p-10 bg-grey-500 rounded-b-lg lg:relative lg:rounded-lg overflow-hidden"
-      style={{
-        background:
-          "url(/images/illustration-authentication.svg) no-repeat top center",
-      }}
-    >
+    <header className="bg-grey-500">
       <Link
         to="/"
         className="block w-fit mx-auto lg:mx-[unset] lg:absolute z-10 p-5"
         role="link"
         aria-label="Go to homepage"
       >
-        <img src="/images/logo-large.svg" alt="PersonalFinance App Logo" />
+      <img src="/images/logo-large.svg" alt="PersonalFinance App Logo" />
       </Link>
-      {!loggedIn ? (
-        <div className="relative h-fit place-content-end">
-          <Heading variant="heading_lg" className="text-white">
-            Keep track of your money and save for your future
-          </Heading>
-          <p className="text-white">
-            Personal finance app puts you in control of your spending. Track
-            transactions, set budgets, and add to savings pots easily.
-          </p>
-        </div>
-      ) : (
-        <MainNav />
-      )}
     </header>
   );
 };
