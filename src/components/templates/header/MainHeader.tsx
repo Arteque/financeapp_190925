75a51fd6390615fc69134be572bfe63afd0d3874
Ahmed Lemssiah <art3que@gmail.com>
2025-12-23 15:58:56 +0100

    Header Styling cross view ports

--- src/components/templates/header/MainHeader.tsx
+++ src/components/templates/header/MainHeader.tsx
@@ -9,8 +9,9 @@ const MainHeader = () => {
 
   return (
     <header className="bg-grey-500 fixed top-0 w-full z-100 
-    lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:right-[unset] lg:h-screen lg:max-w-[600px] lg:rounded-lg lg:m-5">
-      <Link
+    lg:sticky lg:top-0 lg:bottom-0 lg:left-0 lg:max-w-[600px] lg:rounded-lg lg:m-10">
+      
+        <Link
         to="/"
         className="block w-fit mx-auto lg:mx-[unset] lg:absolute z-10 p-5"
         role="link"
@@ -23,7 +24,7 @@ const MainHeader = () => {
       {
         !isLogged ? (
             <figure className="hidden lg:block">
-              <img src="/images/illustration-authentication.svg" alt="Authentication Illustration" />
+              <img src="/images/illustration-authentication.svg" alt="Authentication Illustration" className="w-full h-full object-contain"/>
               <figcaption>
                 <h2>
                   Keep track of your money
@@ -38,6 +39,7 @@ const MainHeader = () => {
           <MainNav />
         )
       }    
+      
     </header>
   );
 };
